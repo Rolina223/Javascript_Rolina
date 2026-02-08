4a3049db77bd6e9eb2282b5f71416cbf43d0a1b7
Rolina223 <gopalireb223@gmail.com>
2026-02-08 17:51:10 +0530

    Update 02_DataTypes.js

--- JS_ROLINA/02_DataTypes.js
+++ JS_ROLINA/02_DataTypes.js
@@ -6,15 +6,43 @@ let age = 19;
 let name = "rolina";
 let isLoggedIn = true;
 
+let val = undefined;
+
 // Data Types in JavaScript
+//Primitive data types
 
 // number   -> 2 to the power 53
 // bigint
 // string  -> ""
 // boolean -> true / false
-// null    -> standalone value (typeof null is object)
-// undefined -> value is not assigned
-// symbol  -> used for uniqueness
+// null    -> standalone value (typeof null is object) //Null
+// undefined -> value is not assigned  //Undefined
+// symbol  -> used for uniqueness - Datatype - symbol
 
 console.log(typeof isLoggedIn); // boolean
 console.log("hello");
+
+//Non - Primitive 
+//Arrays
+//Functions
+//Object
+
+// For non primitive type of datatype is object
+
+let hero = ["A","b","c"]
+
+let myObj = {
+     name:"Rolina",
+     age: 27
+}
+
+ function myfunc()
+{
+    console.log("Hello")
+}
+
+console.log(typeof hero)
+console.log(typeof myObj)
+console.log(typeof myfunc)
+
+
