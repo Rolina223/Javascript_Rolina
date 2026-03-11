82ef67fd94b6570e14f4497d3691bf8a93c8c6ba
Rolina223 <gopalireb223@gmail.com>
2026-03-11 15:52:21 +0530

    Update IIFE.js

--- 03_Basics/IIFE.js
+++ 03_Basics/IIFE.js
@@ -1,10 +1,12 @@
 //IIFE -> Immediaetly Invoked Function Expression
 
+//Namely IIFE
 (function coffee(){
     console.log("Coffee");
     
 })();
 
+//Arrow IIFE    
 ( (value) => {
     console.log("Coffe Second with ", `${value}`);
     
